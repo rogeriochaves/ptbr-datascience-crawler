83f73758609f0da57b01a5964dbda97d5be96e3d
Rogerio Chaves <rogeriocfj@gmail.com>
2019-05-01 01:18:06 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from github import Github, InputFileContent
+from telethon import TelegramClient, events, sync
+import re
+import os
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+api_id = os.environ['API_ID']
+api_hash = os.environ['API_HASH']
+github_token = os.environ['GITHUB_TOKEN']
+
+
+def get_gist():
+    g = Github(github_token)
+    return g.get_gist("bc1469d0a8ed0de01369aa34be2bad76")
+
+
+def get_last_id(content):
+    matches = re.search(r'Last Message ID: (.*)', content)
+    return int(matches[1])
+
+
+def get_link_messages(client, offset):
+    messages = client.get_messages(
+        'datasciencepython', min_id=offset, limit=100, reverse=True)
+    messages_with_links = filter(lambda m: m.message and re.match(
+        r'.*https?://.*', m.message), messages)
+
+    return list(messages_with_links)
+
+
+def build_links_list(messages):
+    timestamp = messages[-1].date.strftime('%d/%m/%Y %H:%M')
+    last_id = messages[-1].id
+    messages_list = "\n".join(
+        ["- %s" % re.sub(r'\n', '\n  ', m.message) for m in messages])
+    return "### %s\nLast Message ID: %d\n\n%s" % (timestamp, last_id, messages_list)
+
+
+if __name__ == "__main__":
+    client = TelegramClient('session_name', api_id, api_hash)
+    client.start()
+    print("Connected to Telegram!")
+
+    gist = get_gist()
+    file = gist.files['links.md']
+    content = file.content
+    last_id = get_last_id(content)
+    print("Getting messages after", last_id)
+
+    messages = get_link_messages(client, last_id)
+    print(len(messages), "links found")
+
+    new_content = build_links_list(messages)
+    new_file = InputFileContent(new_content + "\n\n" + content)
+
+    print("Saving gist")
+    gist.edit(files={'links.md': new_file})
+
+    print("Done! https://gist.github.com/rogeriochaves/bc1469d0a8ed0de01369aa34be2bad76")
---
+++ tests/test_main.py
@@ -0,0 +1,37 @@
+import unittest
+from main import get_last_id, get_link_messages, build_links_list
+from unittest.mock import Mock
+import datetime
+
+
+class MainTestCase(unittest.TestCase):
+    def test_get_last_id(self):
+        sample = "04/04/2019 17:45\nLast Message ID: 456\n\n- foo\n- bar\nLast Message ID: 123"
+        self.assertEqual(456, get_last_id(sample))
+
+    def test_get_messages(self):
+        mocked_message1 = Mock()
+        mocked_message1.message = "no links"
+        mocked_message2 = Mock()
+        mocked_message2.message = "here is a link: http://google.com"
+
+        mocked_client = Mock()
+        mocked_client.get_messages = Mock(
+            return_value=[mocked_message1, mocked_message2])
+
+        result = get_link_messages(
+            mocked_client, 0)
+        expected = [mocked_message2]
+        self.assertEqual(expected, result)
+
+    def test_build_links_list(self):
+        msg1 = Mock()
+        msg1.message = "muito legal!\nhttp://www.pudim.com.br\nné?"
+        msg2 = Mock()
+        msg2.message = "here is a link: http://google.com"
+        msg2.id = 1001
+        msg2.date = datetime.datetime(2019, 4, 3, 20, 43, 4)
+
+        result = build_links_list([msg1, msg2])
+        expected = "### 03/04/2019 20:43\nLast Message ID: 1001\n\n- muito legal!\n  http://www.pudim.com.br\n  né?\n- here is a link: http://google.com"
+        self.assertEqual(expected, result)
